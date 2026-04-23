25b02c16328bc07a56f74558382910b28e0a84a4
zendrex <email@zendrex.com>
2026-04-23 15:00:48 -0700

    refactor(index): final Stage-3 public API surface

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,38 @@
 /**
  * @packageDocumentation
  *
- * A TypeScript decorator factory library with metadata storage and reflection capabilities.
+ * A TypeScript decorator factory library with metadata storage and reflection
+ * capabilities, built on TC39 Stage-3 decorators.
  */
 /** biome-ignore-all lint/performance/noBarrelFile: main index file */
-import "reflect-metadata";
 
-export { AnnotateError, AnnotateErrorCode, UnregisteredClassError } from "./errors";
+export { AnnotateError, AnnotateErrorCode, DuplicateMetadataError, UnregisteredClassError } from "./errors";
 export { createAccessorInterceptor } from "./factories/accessor-interceptor";
 export { createClassDecorator } from "./factories/class-decorator";
 export { createMethodDecorator } from "./factories/method-decorator";
 export { createMethodInterceptor } from "./factories/method-interceptor";
-export { createParameterDecorator } from "./factories/parameter-decorator";
 export { createPropertyDecorator } from "./factories/property-decorator";
-export {
-	appendMetadata,
-	defineMetadata,
-	getMetadata,
-	getMetadataArray,
-	getOwnMetadata,
-	getParameterMap,
-	setParameterMap,
-} from "./metadata/store";
 export { reflect } from "./reflector/reflector";
 export { materialize } from "./runtime/materialize";
 export type {
+	AccessorInterceptorOptions,
+	AnyFn,
+	DecoratedAccessorFactory,
 	DecoratedClassFactory,
 	DecoratedMethodFactory,
-	DecoratedParameterFactory,
 	DecoratedPropertyFactory,
 	DecoratorOptions,
 	InterceptorContext,
 	MethodInterceptorOptions,
-	ParameterDecoratorOptions,
-	PropertyGetter,
-	PropertyInterceptorOptions,
-	PropertySetter,
 } from "./factories/types";
-export type { MetadataArray, MetadataKey, ParameterMetadataMap } from "./metadata/types";
+export type { MetadataArray, MetadataKey } from "./metadata/types";
 export type { Reflector } from "./reflector/reflector";
 export type {
 	DecoratedClass,
-	DecoratedConstructorParameter,
 	DecoratedItem,
 	DecoratedKind,
 	DecoratedMethod,
-	DecoratedMethodParameter,
 	DecoratedMethodSingle,
-	DecoratedParameter,
 	DecoratedProperty,
 	DecoratedPropertySingle,
 	ScopedReflector,
